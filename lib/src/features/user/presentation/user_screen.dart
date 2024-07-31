0a082ef5a3a15fb55d53e77d2e92d17bbc25a173
Eka Robyanto <robyantoeka@gmail.com>
2024-07-31 23:13:52 +0700

    add contents to user screen

--- lib/src/features/user/presentation/user_screen.dart
+++ lib/src/features/user/presentation/user_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:go_router/go_router.dart';
 import 'package:library_app/src/core/auth/auth_cubit.dart';
+import 'package:library_app/src/features/library/presentation/cubit/library_cubit.dart';
 import 'package:library_app/src/features/user/presentation/widget/user_header.dart';
 import 'package:library_app/src/router/router.dart';
 import 'package:library_app/src/theme/app_theme.dart';
@@ -25,60 +27,53 @@ class UserScreen extends StatelessWidget {
               child: Scrollbar(
                 child: Padding(
                   padding: const EdgeInsets.all(8.0),
-                  child: RefreshIndicator(
-                    onRefresh: () async {},
-                    color: Theme.of(context).primaryColor,
-                    child: SingleChildScrollView(
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          UserHeader(
-                            showLogout: true,
-                            backgroundColor: color.primaryShade,
+                  child: SingleChildScrollView(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        UserHeader(
+                          showLogout: true,
+                          backgroundColor: color.primaryShade,
+                        ),
+                        const SizedBox(
+                          height: 16,
+                        ),
+                        BlocBuilder<LibraryCubit, LibraryState>(
+                          builder: (context, state) => state.maybeWhen(
+                            orElse: () => const SizedBox.shrink(),
+                            success: (results) {
+                              final sortedList = List.from(results)
+                                ..sort((a, b) => a.title.compareTo(b.title));
+                              return RefreshIndicator(
+                                onRefresh: () async =>
+                                    context.read<LibraryCubit>().getBooks(),
+                                color: Theme.of(context).primaryColor,
+                                child: ListView.separated(
+                                  itemCount: sortedList.length,
+                                  shrinkWrap: true,
+                                  itemBuilder: (context, index) =>
+                                      HorizontalBookList(
+                                    showAll: true,
+                                    label: sortedList[index].title,
+                                    books: sortedList[index].books,
+                                    showAllCallback: () async {
+                                      await context.push(
+                                        '/book-list',
+                                        extra: {
+                                          'title': sortedList[index].title,
+                                          'url': sortedList[index].url,
+                                        },
+                                      );
+                                    },
+                                  ),
+                                  separatorBuilder: (context, index) =>
+                                      const SizedBox(height: 12),
+                                ),
+                              );
+                            },
                           ),
-                          const SizedBox(
-                            height: 16,
-                          ),
-                          HorizontalBookList(
-                            showAll: true,
-                            label: "My Books",
-                            canUploadBook: true,
-                            showAllCallback: () =>
-                                router.push('/book-list', extra: {
-                              'title': 'My Books',
-                              'url': '/my-books',
-                            }),
-                            books: const [],
-                          ),
-                          const SizedBox(
-                            height: 16,
-                          ),
-                          HorizontalBookList(
-                            showAll: true,
-                            canUploadBook: true,
-                            label: "Favorite Books",
-                            showAllCallback: () =>
-                                router.push('/book-list', extra: {
-                              'title': 'Favorite Books',
-                              'url': '/favorite-books',
-                            }),
-                            books: const [],
-                          ),
-                          const SizedBox(
-                            height: 16,
-                          ),
-                          HorizontalBookList(
-                            label: "Recently Read Books",
-                            showAll: true,
-                            showAllCallback: () =>
-                                router.push('/book-list', extra: {
-                              'title': 'Recently Read',
-                              'url': '/recently-read',
-                            }),
-                            books: const [],
-                          ),
-                        ],
-                      ),
+                        ),
+                      ],
                     ),
                   ),
                 ),
